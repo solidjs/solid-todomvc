8b00ab5f2c1156f138f5d329db204255b4bac1b1
Ryan Carniato <ryansolid@gmail.com>
2019-06-08 22:25:16 -0700

    switch to rollup

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+const plugins = [
+	babel({
+		exclude: 'node_modules/**',
+		plugins: [["jsx-dom-expressions", { moduleName: 'solid-js/dom' }]]
+	}),
+	resolve({ extensions: ['.js', '.jsx'] })
+];
+
+if (process.env.production) {
+	plugins.push(terser());
+}
+
+export default {
+	input: 'src/index.jsx',
+	output: {
+		file: 'dist/bundle.js',
+		format: 'iife'
+	},
+	plugins
+};
