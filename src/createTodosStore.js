12d4d09f5444476496bde8de02c9c5817e2dda0a
Ryan Carniato <ryansolid@gmail.com>
2019-10-18 08:23:10 -0700

    update to solid 0.12.0

--- src/createTodosStore.js
+++ src/createTodosStore.js
@@ -1,6 +1,6 @@
-import { createState, createEffect } from 'solid-js';
+import { createState, createEffect } from "solid-js";
 
-const LOCAL_STORAGE_KEY = 'todos-solid';
+const LOCAL_STORAGE_KEY = "todos-solid";
 
 function createLocalState(value) {
   // load stored todos on init
@@ -8,27 +8,50 @@ function createLocalState(value) {
     [state, setState] = createState(stored ? JSON.parse(stored) : value);
 
   // JSON.stringify creates deps on every iterable field
-  createEffect(() => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state)));
+  createEffect(() =>
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state))
+  );
   return [state, setState];
 }
 
 export default function createTodosStore() {
-  const [state, setState] = createLocalState({counter: 1, todos: [], showMode: 'all'});
+  const [state, setState] = createLocalState({
+    counter: 1,
+    todos: [],
+    showMode: "all"
+  });
   createEffect(() => {
     const completedCount = state.todos.filter(todo => todo.completed).length;
-    setState({ completedCount, remainingCount: state.todos.length - completedCount });
+    setState({
+      completedCount,
+      remainingCount: state.todos.length - completedCount
+    });
   });
 
   return [
-    state, {
-    addTodo: ({title}) => setState(
-      ['todos', t => [{title, id: state.counter, completed: false}, ...t]],
-      ['counter', c => c + 1]
-    ),
-    removeTodo: todoId => setState('todos', t => t.filter(item => item.id !== todoId)),
-    editTodo: todo => setState('todos', state.todos.findIndex(item => item.id === todo.id), todo),
-    clearCompleted: () => setState('todos', t => t.filter(todo => !todo.completed)),
-    toggleAll: completed => setState('todos', todo => todo.completed !== completed, {completed}),
-    setVisibility: showMode => setState('showMode', showMode)
-  }];
+    state,
+    {
+      addTodo: ({ title }) =>
+        setState(
+          [
+            "todos",
+            t => [{ title, id: state.counter, completed: false }, ...t]
+          ],
+          ["counter", c => c + 1]
+        ),
+      removeTodo: todoId =>
+        setState("todos", t => t.filter(item => item.id !== todoId)),
+      editTodo: todo =>
+        setState(
+          "todos",
+          state.todos.findIndex(item => item.id === todo.id),
+          todo
+        ),
+      clearCompleted: () =>
+        setState("todos", t => t.filter(todo => !todo.completed)),
+      toggleAll: completed =>
+        setState("todos", todo => todo.completed !== completed, { completed }),
+      setVisibility: showMode => setState("showMode", showMode)
+    }
+  ];
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,124 +1,189 @@
-import { createState, onCleanup } from 'solid-js';
-import { render, selectWhen } from 'solid-js/dom';
-import createTodosStore from './createTodosStore';
+import { createState, onCleanup } from "solid-js";
+import { render, selectWhen } from "solid-js/dom";
+import createTodosStore from "./createTodosStore";
 
 const ESCAPE_KEY = 27,
   ENTER_KEY = 13;
 
-const setFocus = (el) => Promise.resolve().then(() => el.focus());
+const setFocus = el => Promise.resolve().then(() => el.focus());
 
 const TodoApp = () => {
-  const [store, {addTodo, toggleAll, editTodo, removeTodo, clearCompleted, setVisibility}] = createTodosStore(),
-    locationHandler = () => setVisibility(location.hash.slice(2) || 'all');
+  const [
+      store,
+      {
+        addTodo,
+        toggleAll,
+        editTodo,
+        removeTodo,
+        clearCompleted,
+        setVisibility
+      }
+    ] = createTodosStore(),
+    locationHandler = () => setVisibility(location.hash.slice(2) || "all");
 
-  window.addEventListener('hashchange', locationHandler);
-  onCleanup(() => window.removeEventListener('hashchange', locationHandler));
+  window.addEventListener("hashchange", locationHandler);
+  onCleanup(() => window.removeEventListener("hashchange", locationHandler));
 
-  return <section class='todoapp'>
-    <TodoHeader addTodo={addTodo} />
-    <Show when={(store.todos.length > 0)}>
-      <TodoList {...{store, toggleAll, editTodo, removeTodo}}/>
-      <TodoFooter store={store} clearCompleted={clearCompleted} />
-    </Show>
-  </section>
-}
+  return (
+    <section class="todoapp">
+      <TodoHeader addTodo={addTodo} />
+      <Show when={store.todos.length > 0}>
+        <TodoList
+          store={store}
+          toggleAll={toggleAll}
+          editTodo={editTodo}
+          removeTodo={removeTodo}
+        />
+        <TodoFooter store={store} clearCompleted={clearCompleted} />
+      </Show>
+    </section>
+  );
+};
 
-const TodoHeader = ({ addTodo }) =>
-  <header class='header'>
+const TodoHeader = ({ addTodo }) => (
+  <header class="header">
     <h1>todos</h1>
     <input
-      class='new-todo'
-      placeholder='What needs to be done?'
-      onKeyUp={({target, keyCode}) => {
+      class="new-todo"
+      placeholder="What needs to be done?"
+      onKeyUp={({ target, keyCode }) => {
         const title = target.value.trim();
         if (keyCode === ENTER_KEY && title) {
-          addTodo({title});
-          target.value = '';
-       }
-     }}
+          addTodo({ title });
+          target.value = "";
+        }
+      }}
     />
   </header>
+);
 
 const TodoList = ({ store, editTodo, removeTodo, toggleAll }) => {
   const [state, setState] = createState(),
     filterList = todos => {
-      if (store.showMode === 'active') return todos.filter(todo => !todo.completed);
-      else if (store.showMode === 'completed') return todos.filter(todo => todo.completed);
-      else return todos
+      if (store.showMode === "active")
+        return todos.filter(todo => !todo.completed);
+      else if (store.showMode === "completed")
+        return todos.filter(todo => todo.completed);
+      else return todos;
     },
     isEditing = todoId => state.editingTodoId === todoId,
-    setCurrent = todoId => setState('editingTodoId', todoId),
-    save = ({target: {value}}, todoId) => {
+    setCurrent = todoId => setState("editingTodoId", todoId),
+    save = ({ target: { value } }, todoId) => {
       const title = value.trim();
       if (state.editingTodoId === todoId && title) {
-        editTodo({id: todoId, title});
+        editTodo({ id: todoId, title });
         setCurrent();
-     }
-   },
-    toggle = ({target: {checked}}, todoId) => editTodo({id: todoId, completed: checked}),
+      }
+    },
+    toggle = ({ target: { checked } }, todoId) =>
+      editTodo({ id: todoId, completed: checked }),
     edit = (e, todoId) => setCurrent(todoId),
     remove = (e, todoId) => removeTodo(todoId),
     doneEditing = (e, todoId) => {
       if (e.keyCode === ENTER_KEY) save(e, todoId);
       else if (e.keyCode === ESCAPE_KEY) setCurrent();
-    };
+    },
+    applyClass = selectWhen(() => state.editingTodoId, "editing");
 
-  return <section class='main'>
-    <input
-      id='toggle-all'
-      class='toggle-all'
-      type='checkbox'
-      checked={(!store.remainingCount)}
-      onInput={({target: {checked}}) => toggleAll(checked)}
-    />
-    <label for='toggle-all' />
-    <ul class='todo-list'>
-      <For each={(filterList(store.todos))}
-        transform={selectWhen(() => state.editingTodoId, 'editing')}
-      >{
-          todo => <TodoItem {...{todo, isEditing, toggle, edit, remove, doneEditing, save}} />
-       }</For>
-    </ul>
-  </section>
-}
-
-const TodoItem = ({ todo, isEditing, toggle, edit, remove, save, doneEditing }) =>
-  <li class='todo' model={todo.id} classList={({completed: todo.completed})}>
-    <div class='view'>
+  return (
+    <section class="main">
       <input
-        class='toggle'
-        type='checkbox'
-        checked={(todo.completed)}
-        onInput={toggle}
+        id="toggle-all"
+        class="toggle-all"
+        type="checkbox"
+        checked={!store.remainingCount}
+        onInput={({ target: { checked } }) => toggleAll(checked)}
       />
-      <label onDblClick={edit}>{(todo.title)}</label>
-      <button class='destroy' onClick={remove} />
-    </div>
-    <Show when={(isEditing(todo.id))}>
-      <input
-        class='edit'
-        value={todo.title}
-        onFocusOut={save}
-        onKeyUp={doneEditing}
-        forwardRef={setFocus}
-      />
-    </Show>
-  </li>
+      <label for="toggle-all" />
+      <ul class="todo-list">
+        <For each={filterList(store.todos)} transform={applyClass}>
+          {todo => (
+            <TodoItem
+              todo={todo}
+              isEditing={isEditing}
+              toggle={toggle}
+              edit={edit}
+              remove={remove}
+              doneEditing={doneEditing}
+              save={save}
+            />
+          )}
+        </For>
+      </ul>
+    </section>
+  );
+};
 
-const TodoFooter = ({ store, clearCompleted }) =>
-  <footer class='footer'>
-    <span class='todo-count'>
-      <strong>{(store.remainingCount)}</strong>
-      {(store.remainingCount === 1 ? ' item' : ' items')} left
+const TodoItem = ({
+  todo,
+  isEditing,
+  toggle,
+  edit,
+  remove,
+  save,
+  doneEditing
+}) => {
+  const todoTitle = todo.title;
+  return (
+    <li class="todo" model={todo.id} classList={{ completed: todo.completed }}>
+      <div class="view">
+        <input
+          class="toggle"
+          type="checkbox"
+          checked={todo.completed}
+          onInput={toggle}
+        />
+        <label onDblClick={edit}>{todo.title}</label>
+        <button class="destroy" onClick={remove} />
+      </div>
+      <Show when={isEditing(todo.id)}>
+        <input
+          class="edit"
+          value={todoTitle}
+          onFocusOut={save}
+          onKeyUp={doneEditing}
+          forwardRef={setFocus}
+        />
+      </Show>
+    </li>
+  );
+};
+
+const TodoFooter = ({ store, clearCompleted }) => (
+  <footer class="footer">
+    <span class="todo-count">
+      <strong>{store.remainingCount}</strong>
+      {store.remainingCount === 1 ? " item" : " items"} left
     </span>
-    <ul class='filters'>
-      <li><a href='#/' classList={({selected: store.showMode === 'all'})}>All</a></li>
-      <li><a href='#/active' classList={({selected: store.showMode === 'active'})}>Active</a></li>
-      <li><a href='#/completed' classList={({selected: store.showMode === 'completed'})}>Completed</a></li>
+    <ul class="filters">
+      <li>
+        <a href="#/" classList={{ selected: store.showMode === "all" }}>
+          All
+        </a>
+      </li>
+      <li>
+        <a
+          href="#/active"
+          classList={{ selected: store.showMode === "active" }}
+        >
+          Active
+        </a>
+      </li>
+      <li>
+        <a
+          href="#/completed"
+          classList={{ selected: store.showMode === "completed" }}
+        >
+          Completed
+        </a>
+      </li>
     </ul>
-    <Show when={(store.completedCount > 0)}>
-      <button class='clear-completed' onClick={clearCompleted}>Clear completed</button>
+    <Show when={store.completedCount > 0}>
+      <button class="clear-completed" onClick={clearCompleted}>
+        Clear completed
+      </button>
     </Show>
   </footer>
+);
 
-render(TodoApp, document.getElementById('main'));
+render(TodoApp, document.getElementById("main"));
