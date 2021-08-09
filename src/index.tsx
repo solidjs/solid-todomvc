0409410f56c6c334be25891ae73f82f59b3c3e9b
devinrhode2 <devinrhode2+github@gmail.com>
2021-08-08 22:59:37 -0500

    convert to TS WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,69 +1,155 @@
-import { createMemo, createEffect, onCleanup } from "solid-js"
-import { createStore } from "solid-js/store"
+import { createMemo, createEffect, onCleanup, Show, For } from "solid-js"
+import { createStore, StoreNode, SetStoreFunction } from "solid-js/store"
 import { render } from "solid-js/web"
 import type { Component } from "solid-js"
 
 const ESCAPE_KEY = 27
 const ENTER_KEY = 13
 
-// const setFocus = (el) => setTimeout(() => el.focus());
+// @ts-expect-error - ts(6133) this is actually used below in `use:setFocus`
+const setFocus = (el: HTMLInputElement) => setTimeout(() => el.focus())
 
 const LOCAL_STORAGE_KEY = "todos-solid"
-function createLocalStore(value) {
+const createLocalStore = <T extends StoreNode>(value: T): [T, SetStoreFunction<T>] => {
   // load stored todos on init
   const stored = localStorage.getItem(LOCAL_STORAGE_KEY)
-  const [state, setState] = createStore(stored ? JSON.parse(stored) : value)
+  const [state, setState] = createStore<T>(stored ? JSON.parse(stored) : value)
 
-  // JSON.stringify creates deps on every iterable field
-  createEffect(() => localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state)))
+  createEffect(() => {
+    // JSON.stringify creates deps on every iterable field
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state))
+  })
   return [state, setState]
 }
 
 const TodoApp: Component = () => {
+  // Are the typescript types lame, or is this type a bad idea?
+  interface SolidInputEvent extends InputEvent {
+    currentTarget: HTMLInputElement // // TODO convert underlying declaration to an `interface` - assuming this code exists somewhere :)
+    target: Element
+  }
+  interface SolidKeyboardEvent extends KeyboardEvent {
+    currentTarget: HTMLInputElement
+    target: Element // Making this `HTMLInputElement` creates this error at bottom of file:
+    //  Types of property 'target' are incompatible.
+    //    Type 'EventTarget & Element' is not assignable to type 'HTMLInputElement'
+  }
+
+  interface TodoItem {
+    id: TodoID
+    completed?: boolean
+    title?: string // I don't understand why these should be optional
+  }
+  type ShowMode = "all" | "active" | "completed"
+  type ReadonlyTodos = readonly TodoItem[]
+  type Todos = TodoItem[]
+  type TodoID = null | number
+  type StoreState = {
+    counter: number
+    todos: Todos // Making this ReadonlyTodos causes some odd error inside editTodo
+    showMode: ShowMode
+    editingTodoId: TodoID
+  }
   const [state, setState] = createLocalStore({
     counter: 1,
     todos: [],
     showMode: "all",
     editingTodoId: null,
+  } as StoreState)
+  const selectCompleted = (todos: ReadonlyTodos) => todos.filter((todo) => todo.completed)
+  const selectIncomplete = (todos: ReadonlyTodos) => todos.filter((todo) => !todo.completed)
+  const remainingCount = createMemo(() => {
+    return state.todos.length - selectCompleted(state.todos).length
   })
-  const remainingCount = createMemo(
-    () => state.todos.length - state.todos.filter((todo) => todo.completed).length
-  )
-  const filterList = (todos) => {
-    if (state.showMode === "active") return todos.filter((todo) => !todo.completed)
-    else if (state.showMode === "completed") return todos.filter((todo) => todo.completed)
-    else return todos
-  }
-  const removeTodo = (todoId) => setState("todos", (t) => t.filter((item) => item.id !== todoId))
-  const editTodo = (todo) => setState("todos", (item) => item.id === todo.id, todo)
-  const clearCompleted = () => setState("todos", (t) => t.filter((todo) => !todo.completed))
-  const toggleAll = (completed) =>
-    setState("todos", (todo) => todo.completed !== completed, { completed })
-  const setEditing = (todoId) => setState("editingTodoId", todoId)
-  const addTodo = ({ target, keyCode }) => {
-    const title = target.value.trim()
-    if (keyCode === ENTER_KEY && title) {
-      setState({
-        todos: [{ title, id: state.counter, completed: false }, ...state.todos],
-        counter: state.counter + 1,
-      })
-      target.value = ""
-    }
-  }
-  const save = (todoId, { target: { value } }) => {
-    const title = value.trim()
-    if (state.editingTodoId === todoId && title) {
-      editTodo({ id: todoId, title })
-      setEditing()
-    }
-  }
-  const toggle = (todoId, { target: { checked } }) => editTodo({ id: todoId, completed: checked })
-  const doneEditing = (todoId, e) => {
-    if (e.keyCode === ENTER_KEY) save(todoId, e)
-    else if (e.keyCode === ESCAPE_KEY) setEditing()
+
+  const removeTodo = (id: number) => {
+    // Two args
+    setState(
+      "todos",
+      (
+        todos: ReadonlyTodos // odd
+      ) => {
+        return todos.filter(
+          (
+            todo // : TodoItem // Type was pretty odd here - ideally don't need `: TodoItem`
+          ) => {
+            return todo.id !== id
+          }
+        )
+      }
+    )
   }
 
-  const locationHandler = () => setState("showMode", location.hash.slice(2) || "all")
+  const clearCompleted = () => {
+    // Two args
+    setState(
+      "todos",
+      (
+        todos: readonly TodoItem[] // readonly?
+      ) => todos.filter((todo) => !todo.completed)
+    )
+  }
+
+  const editTodo = (newTodo: TodoItem) => {
+    // Three args
+    setState(
+      "todos",
+      (
+        existingTodo // TodoItem
+      ) => {
+        return existingTodo.id === newTodo.id
+      },
+      newTodo // Hard to know what this is without reading docs. JSDocs on hover would be good.
+    )
+  }
+  const toggleAll = ({ target }: InputEvent) => {
+    const completed = (target as HTMLInputElement).checked
+
+    // Three args
+    setState(
+      "todos",
+      (
+        todo // TodoItem
+      ) => {
+        return todo.completed !== completed
+      },
+      { completed }
+    )
+  }
+
+  const setEditing = (todoId: TodoID) => {
+    // Two args
+    setState("editingTodoId", todoId)
+  }
+  const save = (todoId: TodoID, { target }: SolidKeyboardEvent) => {
+    const { value } = target
+    const title = value.trim()
+    if (state.editingTodoId === todoId && title && todoId !== null) {
+      editTodo({
+        id: todoId,
+        title,
+      })
+      setEditing(todoId)
+    }
+  }
+  const toggle = (todoId: TodoID, { target }: SolidInputEvent) => {
+    editTodo({
+      id: todoId,
+      // TODO: should not need `as HTMLInputElement`
+      completed: (target as HTMLInputElement).checked,
+    })
+  }
+  const doneEditing = (todoId: TodoID, event: SolidKeyboardEvent) => {
+    if (event.keyCode === ENTER_KEY) save(todoId, event)
+    else if (event.keyCode === ESCAPE_KEY) setEditing(todoId)
+  }
+
+  const locationHandler = () => {
+    // TODO: validate this is actually a valid showMode
+    // Probably we want to move the base `type ShowMode` to be a javascript const thing
+    const showMode = window.location.hash.slice(2) as ShowMode
+    setState("showMode", showMode || "all")
+  }
   window.addEventListener("hashchange", locationHandler)
   onCleanup(() => window.removeEventListener("hashchange", locationHandler))
 
@@ -71,7 +157,28 @@ const TodoApp: Component = () => {
     <section class="todoapp">
       <header class="header">
         <h1>todos</h1>
-        <input class="new-todo" placeholder="What needs to be done?" onKeyDown={addTodo} />
+        <input
+          class="new-todo"
+          placeholder="What needs to be done?"
+          onKeyDown={({ target, keyCode }: SolidKeyboardEvent) => {
+            // TODO: should not need `as HTMLInputElement`
+            const title = (target as HTMLInputElement).value.trim()
+            if (keyCode === ENTER_KEY && title) {
+              setState({
+                todos: [
+                  {
+                    title,
+                    id: state.counter,
+                    completed: false,
+                  },
+                  ...state.todos,
+                ],
+                counter: state.counter + 1,
+              })
+              ;(target as HTMLInputElement).value = ""
+            }
+          }}
+        />
       </header>
 
       <Show when={state.todos.length > 0}>
@@ -81,11 +188,23 @@ const TodoApp: Component = () => {
             class="toggle-all"
             type="checkbox"
             checked={!remainingCount()}
-            onInput={({ target: { checked } }) => toggleAll(checked)}
+            onInput={
+              (a, b, c) => {
+                toggleAll(data, event)
+              },
+            }
           />
           <label for="toggle-all" />
           <ul class="todo-list">
-            <For each={filterList(state.todos)}>
+            <For
+              each={
+                state.showMode === "active"
+                  ? selectIncomplete(state.todos)
+                  : state.showMode === "completed"
+                  ? selectCompleted(state.todos)
+                  : state.todos
+              }
+            >
               {(todo) => (
                 <li
                   class="todo"
@@ -99,17 +218,42 @@ const TodoApp: Component = () => {
                       class="toggle"
                       type="checkbox"
                       checked={todo.completed}
-                      onInput={[toggle, todo.id]}
+                      onInput={[
+                        (data, event) => {
+                          toggle(data, event)
+                        },
+                        todo.id,
+                      ]}
                     />
-                    <label onDblClick={[setEditing, todo.id]}>{todo.title}</label>
+                    <label
+                      onDblClick={[
+                        (data, _event) => {
+                          setEditing(data)
+                        },
+                        todo.id,
+                      ]}
+                    >
+                      {todo.title}
+                    </label>
                     <button class="destroy" onClick={[removeTodo, todo.id]} />
                   </div>
                   <Show when={state.editingTodoId === todo.id}>
                     <input
                       class="edit"
+                      type="text"
                       value={todo.title}
-                      onFocusOut={[save, todo.id]}
-                      onKeyUp={[doneEditing, todo.id]}
+                      onFocusOut={[
+                        (data, event) => {
+                          save(data, event)
+                        },
+                        todo.id,
+                      ]}
+                      onKeyUp={[
+                        (data, event) => {
+                          doneEditing(data, event)
+                        },
+                        todo.id,
+                      ]}
                       use:setFocus
                     />
                   </Show>
@@ -121,7 +265,7 @@ const TodoApp: Component = () => {
 
         <footer class="footer">
           <span class="todo-count">
-            <strong>{remainingCount()}</strong> {remainingCount() === 1 ? " item " : " items "} left
+            <strong>{remainingCount()}</strong> item{remainingCount() === 1 ? "" : "s"} left
           </span>
           <ul class="filters">
             <li>
@@ -151,4 +295,4 @@ const TodoApp: Component = () => {
   )
 }
 
-render(TodoApp, document.getElementById("main"))
+render(() => <TodoApp />, document.getElementById("root") as HTMLDivElement)
